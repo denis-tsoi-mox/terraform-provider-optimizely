415d9066100b821632a36336c8aa6a8ef32b6821
dusan.plavak <dusan.plavak@projectdrgn.com>
2022-09-29 17:33:35 +0200

    Add retry functionality on 429 & 5xx errors

--- optimizely/provider.go
+++ optimizely/provider.go
@@ -2,12 +2,14 @@ package optimizely
 
 import (
 	"context"
+	"net/http"
 
 	"github.com/dusan-dragon/terraform-provider-optimizely/optimizely/audience"
 	"github.com/dusan-dragon/terraform-provider-optimizely/optimizely/client"
 	"github.com/dusan-dragon/terraform-provider-optimizely/optimizely/environment"
 	"github.com/dusan-dragon/terraform-provider-optimizely/optimizely/flag"
 	"github.com/dusan-dragon/terraform-provider-optimizely/optimizely/project"
+	"github.com/dusan-dragon/terraform-provider-optimizely/optimizely/transport"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
@@ -22,6 +24,12 @@ func Provider() *schema.Provider {
 			"token": {
 				Type:     schema.TypeString,
 				Required: true,
+			},
+			"http_client_retry_enabled": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("OPTIMIZELY_HTTP_CLIENT_RETRY_ENABLED", true),
+				Description: "Enables request retries on HTTP status codes 429 and 5xx. Defaults to `true`.",
 			},
 		},
 		ResourcesMap: map[string]*schema.Resource{
@@ -41,10 +49,18 @@ func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}
 
 	address := d.Get("host").(string)
 	token := d.Get("token").(string)
+	httpRetryEnabled := d.Get("http_client_retry_enabled").(bool)
+
+	httpClient := http.DefaultClient
+	if httpRetryEnabled {
+		customTransport := transport.NewCustomTransport(httpClient.Transport)
+		httpClient.Transport = customTransport
+	}
 
 	optimizelyClient := client.OptimizelyClient{
-		Address: address,
-		Token:   token,
+		Address:    address,
+		Token:      token,
+		HttpClient: *httpClient,
 	}
 
 	return optimizelyClient, diags
